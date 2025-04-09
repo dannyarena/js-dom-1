0a59fb673fa724e2c96bdc6ac4e13793a8bb1b55
dannyarena <arenadanny71@gmail.com>
2025-04-09 19:55:13 +0200

    Add click event to toggle lamp image and button text

--- img/script.js
+++ img/script.js
@@ -1,2 +1,13 @@
 const img = document.getElementById('lampadina');
 const button = document.getElementById('bottone');
+
+
+button.addEventListener('click', function () {
+  if (button.textContent.includes("Accendi")) {
+    img.src = "img\yellow_lamp.png";
+    button.textContent = "Spegni";
+  } else {
+    img.src = "img\white_lamp.png";
+    button.textContent = "Accendi";
+  }
+});
