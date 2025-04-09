73c76d82e78e85f2baa2505205ee7636bcfbd0d7
dannyarena <arenadanny71@gmail.com>
2025-04-09 20:20:38 +0200

    Fix image path separators in HTML and JavaScript files

--- img/script.js
+++ img/script.js
@@ -4,10 +4,10 @@ const button = document.getElementById('bottone');
 
 button.addEventListener('click', function () {
   if (button.textContent.includes("Accendi")) {
-    img.src = "img\yellow_lamp.png";
+    img.src = "img/yellow_lamp.png";
     button.textContent = "Spegni";
   } else {
-    img.src = "img\white_lamp.png";
+    img.src = "img/white_lamp.png";
     button.textContent = "Accendi";
   }
 });
