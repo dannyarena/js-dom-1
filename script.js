0679c03bbf7ee81d940097fc5b727f12f44429ee
dannyarena <arenadanny71@gmail.com>
2025-04-10 20:50:42 +0200

    Add initial HTML and JavaScript files for lamp toggle functionality

---
+++ script.js
@@ -0,0 +1,13 @@
+const img = document.getElementById('lampadina');
+const button = document.getElementById('bottone');
+
+
+button.addEventListener('click', function () {
+  if (button.textContent.includes("Accendi")) {
+    img.src = "img/img/yellow_lamp.png";
+    button.textContent = "Spegni";
+  } else {
+    img.src = "img/img/white_lamp.png";
+    button.textContent = "Accendi";
+  }
+});
